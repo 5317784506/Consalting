14c7f886707b18e14e38d1c8f588dde6c1290c0b
Roman <yaguar906935@gmail.com>
2023-01-21 23:48:22 +0500

    v1.0

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+
 $(document).ready(function () {
 	$('.header__burger').click(function (event) {
 		$('.header__burger,.header__menu').toggleClass('active');
 	});
+});
+
+
+$(function () {
+	$(".nomber").mask("8(999) 999 99 99");
 });
