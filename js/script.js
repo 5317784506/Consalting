eaba862052c33983b0e9e428fe3336e256839304
Roman <yaguar906935@gmail.com>
2023-01-23 22:06:43 +0500

    js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,38 @@ $(document).ready(function () {
 $(function () {
 	$(".nomber").mask("8(999) 999 99 99");
 });
+
+
+
+
+let a = document.querySelector(".text-tax"),
+	b = document.querySelector(".text-accounting"),
+	c = document.querySelector(".text-lawyer");
+const tax = document.querySelector(".btn-tax"),
+	accounting = document.querySelector(".btn-accounting"),
+	lawyer = document.querySelector(".btn-lawyer");
+
+////////////////////////////////////////////////////
+
+tax.addEventListener('click', function () {
+	a.classList.toggle('active');
+});
+
+/////////////////////////////////////////////////////
+
+accounting.addEventListener('click', function () {
+	b.classList.toggle('active');
+});
+
+lawyer.addEventListener('click', function () {
+	c.classList.toggle('active');
+});
+
+/*
+if (tax) {
+	tax.addEventListener('click', function () {
+		a.classList.toggle('active');
+	}
+	);
+}
+*/
